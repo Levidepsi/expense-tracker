66e816648c52bf7d9d35b010cf81fefb97ddd5ad
Levidepsi <levidepsi7@gmail.com>
2026-02-26 15:14:08 +0800

    first commit

--- script.js
+++ script.js
@@ -201,3 +201,13 @@ const updateTotals = () => {
 // renderTotalExpenses()
 renderItems()
 updateTotals()
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch((err) => console.log("SW registration failed:", err));
+  });
+}
