b86771d25621566666476db08075c61c92f14f4d
Levidepsi <levidepsi7@gmail.com>
2026-02-27 12:03:34 +0800

    add condition to minus savings to total income

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,41 @@
-const CACHE_NAME = "expense-tracker-v2";
+const CACHE_NAME = "expense-tracker-v3";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json"
 ];
 
+// Install
 self.addEventListener("install", (event) => {
+  self.skipWaiting(); // activate immediately
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
 
+// Activate (delete old caches)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim(); // take control immediately
+});
+
+// Fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request)
